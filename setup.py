f8eb7f8a32013ee8c0c8904172c0e4768693c4fe
Marc Pabst <mail@marcpabst.de>
2016-09-17 16:02:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(name='pyyeelight',
-      version='1.0',
+      version='1.2',
       zip_safe=False,
       platforms=["any"],
       packages=find_packages(),
